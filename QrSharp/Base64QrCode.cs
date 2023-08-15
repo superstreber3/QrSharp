b4466997767f797359cab9980cf40e546181bf6b
hakim.liso <hakim.liso@softbauware.de>
2023-08-15 17:23:20 +0200

    possibility to add logo to base64qrcode

--- QrSharp/Base64QrCode.cs
+++ QrSharp/Base64QrCode.cs
@@ -30,7 +30,76 @@ public class Base64QrCode : AbstractQrCode, IDisposable
         return base64;
     }
 
-    private static string BitmapToBase64(SKBitmap bmp)
+	public string GetGraphic(int pixelsPerModule, SKColor darkColor, SKColor lightColor, SKBitmap? icon = null,
+	  int iconSizePercent = 15, int iconBorderWidth = 0, bool drawQuietZones = true,
+	  SKColor? iconBackgroundColor = null)
+	{
+		var size = (QrCodeData.ModuleMatrix.Count - (drawQuietZones ? 0 : 8)) * pixelsPerModule;
+		var offset = drawQuietZones ? 0 : 4 * pixelsPerModule;
+		var base64 = string.Empty;
+		var bmp = new SKBitmap(size, size);
+		using var canvas = new SKCanvas(bmp);
+		var lightPaint = new SKPaint { Color = lightColor };
+		var darkPaint = new SKPaint { Color = darkColor };
+		var drawIconFlag = icon is not null && iconSizePercent is > 0 and <= 100;
+
+		canvas.Clear(lightColor);
+
+		for (var x = 0; x < size + offset; x += pixelsPerModule)
+		{
+			for (var y = 0; y < size + offset; y += pixelsPerModule)
+			{
+				var module =
+					QrCodeData.ModuleMatrix[(y + pixelsPerModule) / pixelsPerModule - 1][
+						(x + pixelsPerModule) / pixelsPerModule - 1];
+				var modulePaint = module ? darkPaint : lightPaint;
+
+				canvas.DrawRect(
+					new SKRect(x - offset, y - offset, x - offset + pixelsPerModule, y - offset + pixelsPerModule),
+					modulePaint);
+			}
+		}
+
+		if (!drawIconFlag)
+		{
+			base64 = BitmapToBase64(bmp);
+			return base64;
+		}
+
+		var iconDestWidth = iconSizePercent * bmp.Width / 100f;
+		var iconDestHeight = iconDestWidth * icon!.Height / icon.Width;
+		var iconX = (bmp.Width - iconDestWidth) / 2;
+		var iconY = (bmp.Height - iconDestHeight) / 2;
+
+		var centerDest = new SKRect(iconX - iconBorderWidth, iconY - iconBorderWidth,
+			iconX + iconDestWidth + iconBorderWidth, iconY + iconDestHeight + iconBorderWidth);
+		var iconDestRect = new SKRect(iconX, iconY, iconX + iconDestWidth, iconY + iconDestHeight);
+
+		var iconBgPaint = iconBackgroundColor is not null
+			? new SKPaint { Color = iconBackgroundColor.Value }
+			: lightPaint;
+
+		if (iconBorderWidth > 0)
+		{
+			using var iconPath = CreateRoundedRectanglePath(centerDest, iconBorderWidth * 2);
+			canvas.DrawPath(iconPath, iconBgPaint);
+		}
+
+		canvas.DrawBitmap(icon, iconDestRect);
+		base64 = BitmapToBase64(bmp);
+
+		return base64;
+	}
+
+	internal static SKPath CreateRoundedRectanglePath(SKRect rect, int cornerRadius)
+	{
+		var roundedRect = new SKPath();
+		roundedRect.AddRoundRect(rect, cornerRadius, cornerRadius);
+		return roundedRect;
+	}
+
+
+	private static string BitmapToBase64(SKBitmap bmp)
     {
         using var data = SKImage.FromBitmap(bmp).Encode(SKEncodedImageFormat.Png, 100);
         var base64 = Convert.ToBase64String(data.ToArray());
